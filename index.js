9c5b1e35a3ef3e514ee77592c5c547470af2ef3f
Patrick da Conceição Silva <151814907+patrickconceicaosilva@users.noreply.github.com>
2025-08-24 13:48:39 -0300

    Fixed Bugs

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ const playPause = document.getElementById("playPause");
 const audio = document.getElementById("audio")
 
 window.addEventListener("load", () => {
-    const audio = window.getElementById("audio")
-    audio.play()
+    audio.pause();
 })
 
 playPause.addEventListener("click", () => {
     if (audio.paused) {
         audio.play();
         playPause.innerHTML = '<i class="fa-solid fa-pause fa-2xl"></i>';
-        playPause.classList.add("pause")
+        playPause.classList.add("pause");
     } else {
         audio.pause();
-        playPause.innerHTML = '<i class="fa-solid fa-play fa-2xl"></i>'
-        playPause.classList.remove("pause")
+        playPause.innerHTML = '<i class="fa-solid fa-play fa-2xl"></i>';
+        playPause.classList.remove("pause");
     }
 });
